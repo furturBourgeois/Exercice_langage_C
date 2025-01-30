8eb0a1cd6f7641834e877880430cf107ae51e059
Ernest BAKOUTA <ernestbakouta01@gmail.com>
2025-01-30 11:02:46 +0100

    mes premier tp test

---
+++ TP1/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//commentaire
+
+/*
+ce code est juste un  TP test
+
+*/
+int main()
+{
+    printf("TP 1, \"syntaxe et structure\" \n \t Hello word\n");
+    printf("ernest, gloire");
+    return 0;
+}
